9e30b07ba38db3d7043bbd31fbd188185c35d5d2
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-22 22:36:53 +0100

    Openpyxl displaying charts.

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,4 +1,5 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
@@ -16,4 +17,15 @@ for row in range(2, sheet.max_row + 1):
 
 new_price_column = sheet.cell(1, 4)
 new_price_column.value = 'new_price'
+
+values = Reference(sheet,
+          min_row=2,
+          max_row=sheet.max_row,
+          min_col=4,
+          max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
+
 wb.save('transactions2.xlsx')
