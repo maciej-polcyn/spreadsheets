bf7d85d7975ec3cdf0e2905cb259bec45a71cf40
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-22 22:44:40 +0100

    Openpyxl cleaning code.

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,31 +1,28 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
-cell = sheet['a1']
-# sheet.cell(1, 1)
 
-# print(cell.value)
-# print(sheet.max_row)
+def process_workbook(filename, sheetname):
+    wb = xl.load_workbook(filename)
+    sheet = wb[sheetname]
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    new_price = cell.value * 0.9
-    new_price_cell = sheet.cell(row, 4)
-    new_price_cell.value = new_price
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        new_price = cell.value * 0.9
+        new_price_cell = sheet.cell(row, 4)
+        new_price_cell.value = new_price
 
-new_price_column = sheet.cell(1, 4)
-new_price_column.value = 'new_price'
+    new_price_column = sheet.cell(1, 4)
+    new_price_column.value = 'new_price'
 
-values = Reference(sheet,
-          min_row=2,
-          max_row=sheet.max_row,
-          min_col=4,
-          max_col=4)
+    values = Reference(sheet,
+              min_row=2,
+              max_row=sheet.max_row,
+              min_col=4,
+              max_col=4)
 
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'e2')
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
 
-wb.save('transactions2.xlsx')
+    wb.save(filename)
